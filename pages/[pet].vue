f546fdc456cec35a99f9c6301ec11631e40f18a3
Juan Víctor Jiménez Corzo <38294666+jvjc@users.noreply.github.com>
2025-09-09 09:36:28 -0600

    Add files via upload

---
+++ pages/[pet].vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>{{ $t(route.params.pet) }}</h1>
+        <div class="d-flex justify-center">
+            <div>
+                <v-img :src="data.thumbnail.source" :width="data.thumbnail.width"></v-img>
+            </div>
+        </div>
+        <h3>{{ data.title }}</h3>
+        <p>{{ data.extract }}</p>
+    </div>
+</template>
+<script setup>
+const route = useRoute();
+const { t, locale } = useI18n();
+const { data, refresh } = await useFetch(`https://${locale.value}.wikipedia.org/api/rest_v1/page/summary/${t(route.params.pet)}`);
+
+watch(locale, () => {
+    console.log('change', locale.value);
+    refresh();
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h2 class="mb-3">{{ $t('subtitle') }}</h2>
+        <p class="mb-2">
+            <v-btn :to="$localePath('/dog')">{{ $t('dog') }}</v-btn>
+        </p>
+        <p><v-btn :to="$localePath('/cat')">{{ $t('cat') }}</v-btn></p>
+    </div>
+</template>
